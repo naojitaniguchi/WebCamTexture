a7119fb354aa569f725f6348a4f829a383f50537
Naoji Taniguchi <naoji.taniguchi@gmail.com>
2017-06-09 07:56:03 +0900

    first commit

---
+++ Assets/WebCamGUITexture.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamGUITexture : MonoBehaviour {
+	GUITexture BackgroundTexture ;
+	WebCamTexture CameraTexture ;
+
+	public void Initialize(){
+		Debug.Log("Initialize");
+		BackgroundTexture = gameObject.AddComponent<GUITexture>();
+		BackgroundTexture.pixelInset = new Rect(0,-1 * Screen.height/2,Screen.width,Screen.height);
+		//set up camera
+		WebCamDevice[] devices = WebCamTexture.devices;
+		string backCamName="";
+		for( int i = 0 ; i < devices.Length ; i++ ) {
+			Debug.Log("Device:"+devices[i].name+ "IS FRONT FACING:"+devices[i].isFrontFacing);
+
+			if (!devices[i].isFrontFacing) {
+				backCamName = devices[i].name;
+			}
+		}
+
+		CameraTexture = new WebCamTexture(backCamName,10000,10000,30);
+		CameraTexture.Play();
+		BackgroundTexture.texture = CameraTexture;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/WebCameTexture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCameTexture : MonoBehaviour {
+	public int Width = 1920;
+	public int Height = 1080;
+	public int FPS = 30;
+
+	void Start () {
+
+		//var euler = transform.localRotation.eulerAngles;
+		//transform.localRotation = Quaternion.Euler( euler.x, euler.y, euler.z - 90 );
+
+		var devices = WebCamTexture.devices;
+		if (devices.Length > 0)
+		{
+			WebCamTexture webcamTexture = new WebCamTexture(Width, Height, FPS);
+			gameObject.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+			webcamTexture.Play();
+		}else
+		{
+			Debug.Log("Webカメラが検出できませんでした");
+			return;
+		}
+	}
+}
