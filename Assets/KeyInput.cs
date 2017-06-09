7022337201e1c8956a79bb6c5e58525bbd68a50c
Naoji Taniguchi <naoji.taniguchi@gmail.com>
2017-06-09 15:48:27 +0900

    added Animation , keyinput .

---
+++ Assets/KeyInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour {
+	public GameObject cubeObject ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown(KeyCode.A) ){
+			Debug.Log( "A pushed" );
+			cubeObject.GetComponent<Animator>().enabled = true ;
+		}
+		if ( Input.GetKeyDown(KeyCode.B) ){
+			Debug.Log( "B pushed" );
+			cubeObject.GetComponent<Animator>().enabled = false ;
+		}
+	}
+}
